12a443c9eee6151a3ef3fe408c7de4883a75e60c
ALAN-CUSTODIO <70667799+ALAN-CUSTODIO@users.noreply.github.com>
2020-09-24 00:17:19 -0300

    Add files via upload

---
+++ nivel.comb.main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+  float atual_comb ;
+    printf("Digite a quantidade de combustivel [Litros]\n");
+   {scanf("%f",&atual_comb);}
+  if (atual_comb>5) 
+{}else if (atual_comb<=5)
+   {printf("Luz de reserva de combustÍvel acesa");}
+    return 0;
+}
